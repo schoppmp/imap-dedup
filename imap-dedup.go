a576e82b05019e80df5e6b74f897894a48af3c9e
Phillipp Schoppmann <schoppmann@informatik.hu-berlin.de>
2016-11-17 15:56:08 +0100

    Add GPL and disclaimer notices

--- imap-dedup.go
+++ imap-dedup.go
@@ -1,3 +1,21 @@
+/*
+	imap-dedup: Removing duplicates in IMAP mailboxes
+	Copyright (C) 2016 Phillipp Schoppmann
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
 package main
 
 import (
@@ -28,6 +46,9 @@ func main() {
 			"Usage: %s [options] [user[:password]@]hostname[:port]/folder\nOptions:\n",
 			os.Args[0])
 		flag.PrintDefaults()
+		fmt.Fprintf(os.Stderr, "\nCopyright (C) 2016 Phillipp Schoppmann\n"+
+			"This is free software; see the source for copying conditions.  There is NO\n"+
+			"warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.\n")
 	}
 	flag.BoolVar(&starttls, "starttls", false, "Use STARTTLS instead of SSL/TLS")
 
