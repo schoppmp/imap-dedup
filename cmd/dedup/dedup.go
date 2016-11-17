fb6b4daef7afac08fc9b098e8cd6341e341114aa
Phillipp Schoppmann <schoppmann@informatik.hu-berlin.de>
2016-11-17 14:44:39 +0100

    Implement deletion of Emails

--- cmd/dedup/dedup.go
+++ cmd/dedup/dedup.go
@@ -9,8 +9,8 @@ import (
 	"os"
 	"time"
 
-	"github.com/howeyc/gopass"
 	"github.com/mxk/go-imap/imap"
+	"golang.org/x/crypto/ssh/terminal"
 )
 
 func check(err error) {
@@ -20,7 +20,7 @@ func check(err error) {
 }
 
 func usage() {
-	fmt.Fprintf(os.Stderr, "Usage: %s [Options] Host\nOptions:\n", os.Args[0])
+	fmt.Fprintf(os.Stderr, "Usage: %s [options] [user[:password]@]hostname[:port]/folder\nOptions:\n", os.Args[0])
 	flag.PrintDefaults()
 }
 
@@ -62,30 +62,37 @@ func main() {
 		password, _ = url.User.Password()
 	}
 	// read username and/or password from STDIN if not given in the URL
+	scanner := bufio.NewScanner(os.Stdin)
 	if username == "" {
-		scanner := bufio.NewScanner(os.Stdin)
 		fmt.Print("Username: ")
 		if scanner.Scan() {
 			username = scanner.Text()
-		} else {
-			check(scanner.Err())
 		}
+		check(scanner.Err())
 	}
 	if password == "" {
 		fmt.Print("Password: ")
-		pass_bytes, err := gopass.GetPasswd()
-		check(err)
-		password = string(pass_bytes)
+		if terminal.IsTerminal(int(os.Stdin.Fd())) { // read password without echo
+			pass_bytes, err := terminal.ReadPassword(int(os.Stdin.Fd()))
+			check(err)
+			password = string(pass_bytes)
+			fmt.Println()
+		} else {
+			if scanner.Scan() {
+				password = scanner.Text()
+			}
+			check(scanner.Err())
+		}
 	}
 	_, err = client.Login(username, password)
 	check(err)
-	_, err = client.Select(url.Path[1:], true)
+	_, err = client.Select(url.Path[1:], false)
 	check(err)
 
 	// get the envelopes of all messages
 	seqSet, err := imap.NewSeqSet("1:*")
 	check(err)
-	cmd, err := imap.Wait(client.UIDFetch(seqSet, "BODY[HEADER]"))
+	cmd, err := imap.Wait(client.Fetch(seqSet, "BODY.PEEK[HEADER]"))
 	check(err)
 	envelopes := map[string]uint32{}
 	toDelete := []uint32{}
@@ -94,12 +101,42 @@ func main() {
 		key := fmt.Sprintf("%s", info.Attrs["BODY[HEADER]"])
 		if _, ok := envelopes[key]; ok {
 			// we already have this message
-			toDelete = append(toDelete, info.UID)
+			toDelete = append(toDelete, info.Seq)
 		} else {
-			envelopes[key] = info.UID
+			envelopes[key] = info.Seq
 		}
 	}
 	fmt.Println("Number of emails:", len(cmd.Data))
-	fmt.Println("Emails kept:", len(envelopes))
-	fmt.Println("Emails to delete:", len(toDelete))
+	fmt.Println("Number of duplicates:", len(toDelete))
+
+	if len(toDelete) == 0 {
+		fmt.Println("No messages to delete")
+		os.Exit(2)
+	}
+	// ask for confirmation
+	s := ""
+	if len(toDelete) != 1 {
+		s = "s"
+	}
+	fmt.Printf("This will delete %d message%s in %s. Do you wish to continue? (y/N)",
+		len(toDelete), s, url.Path[1:])
+	answer := ""
+	if scanner.Scan() {
+		answer = scanner.Text()
+	}
+	check(scanner.Err())
+	if !(answer == "y" || answer == "Y") {
+		fmt.Println("Aborted")
+		os.Exit(2)
+	}
+
+	// mark all messages in toDelete as deleted
+	seqSet = &imap.SeqSet{}
+	seqSet.AddNum(toDelete...)
+	cmd, err = imap.Wait(client.Store(seqSet, "+FLAGS", `\DELETED`))
+	fmt.Printf("%v\n", seqSet)
+	check(err)
+	// close mailbox and expunge
+	_, err = client.Close(true)
+	fmt.Println("Messages successfully deleted")
 }
